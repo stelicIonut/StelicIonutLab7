ba5e549373f0d52d785d02a383c6f36c0a099a43
Ionut Stelic <stelicionut@yahoo.com>
2024-11-25 22:57:35 +0200

    Lab 8 complet

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,31 @@
-﻿namespace StelicIonutLab7
+﻿using System;
+using StelicIonutLab7.Data;
+using System.IO;
+
+
+namespace StelicIonutLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
+          
+
             InitializeComponent();
 
             MainPage = new AppShell();
